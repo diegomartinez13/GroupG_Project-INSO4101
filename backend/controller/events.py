2a3eb935413524729ddc09fc3c22e01c7053a986
Daniel7-28 <juan34daniel@gmail.com>
2023-03-30 20:57:15 -0400

    Added Queries to Users, events and RecyclingCenters

--- backend/controller/events.py
+++ backend/controller/events.py
@@ -47,4 +47,109 @@ class BaseEvents:
             result_list.append(result)
         return jsonify(result_list)
 
-    
+    def getDescriptionByName(self, name):
+        dao = EventDAO()
+        event_list = dao.getDescriptionByEvent(name)
+        result_list = []
+        for row in event_list:
+            result = self.build_map_dict(row)
+            result_list.append(result)
+        return jsonify(result_list)
+
+    def getLocationByName(self, name):
+        dao = EventDAO()
+        event_list = dao.getLocationByName(name)
+        result_list = []
+        for row in event_list:
+            result = self.build_map_dict(row)
+            result_list.append(result)
+        return jsonify(result_list)
+
+    def getStartDateTimeByName(self, name):
+        dao = EventDAO()
+        event_list = dao.getStartDateTimeByName(name)
+        result_list = []
+        for row in event_list:
+            result = self.build_map_dict(row)
+            result_list.append(result)
+        return jsonify(result_list)
+
+    def getNameByLocation(self, location):
+        dao = EventDAO()
+        event_list = dao.getNameByLocation(location)
+        result_list = []
+        for row in event_list:
+            result = self.build_map_dict(row)
+            result_list.append(result)
+        return jsonify(result_list)
+
+    def getNameByStartDateTime(self, start_datetime):
+        dao = EventDAO()
+        event_list = dao.getNameByStartDateTime(start_datetime)
+        result_list = []
+        for row in event_list:
+            result = self.build_map_dict(row)
+            result_list.append(result)
+        return jsonify(result_list)
+
+    def getNameByEndDateTime(self, end_datetime):
+        dao = EventDAO()
+        event_list = dao.getNameByEndDateTime(end_datetime)
+        result_list = []
+        for row in event_list:
+            result = self.build_map_dict(row)
+            result_list.append(result)
+        return jsonify(result_list)
+
+    def updateNameByEvent(self, event_id, json):
+        name = json['name']
+        dao = EventDAO()
+        if name != '':
+            dao.updateNameByEvent(event_id, name)
+        else:
+            return jsonify('Error, we can not change name to \'\'')
+        return jsonify('name Updated'), 200
+
+    def updateDescriptionByEvent(self, event_id, json):
+        description = json['description']
+        dao = EventDAO()
+        if description != '':
+            dao.updateNameByEvent(event_id, description)
+        else:
+            return jsonify('Error, we can not change description to \'\'')
+        return jsonify('description Updated'), 200
+
+    def updateLocationByEvent(self, event_id, json):
+        location = json['location']
+        dao = EventDAO()
+        if location != '':
+            dao.updateNameByEvent(event_id, location)
+        else:
+            return jsonify('Error, we can not change location to \'\'')
+        return jsonify('location Updated'), 200
+
+    def updateStartDateTimeByEvent(self, event_id, json):
+        start_datetime = json['start_datetime']
+        dao = EventDAO()
+        if start_datetime != '':
+            dao.updateNameByEvent(event_id, start_datetime)
+        else:
+            return jsonify('Error, we can not change start_datetime to \'\'')
+        return jsonify('start_datetime Updated'), 200
+
+    def updateEndDateTimeByEvent(self, event_id, json):
+        end_datetime = json['end_datetime']
+        dao = EventDAO()
+        if end_datetime != '':
+            dao.updateNameByEvent(event_id, end_datetime)
+        else:
+            return jsonify('Error, we can not change end_datetime to \'\'')
+        return jsonify('end_datetime Updated'), 200
+
+    def deleteEvent(self, event_id):
+        dao = EventDAO()
+        result = dao.deleteEvent(event_id)
+        if result:
+            return jsonify("DELETED"), 200
+        else:
+            return jsonify("NOT FOUND"), 404
--- backend/model/users.py
+++ backend/model/users.py
@@ -30,6 +30,48 @@ class UserDAO:
         result = cursor.fetchone()
         return result
 
+    def getUsernameById(self, user_id):
+        cursor = self.conn.cursor()
+        query = "select username from users where user_id = %s;"
+        cursor.execute(query, (user_id,))
+        result = cursor.fetchone()
+        return result
+
+    def getPasswordByUsername(self, username):
+        cursor = self.conn.cursor()
+        query = "select password from users where username = %s;"
+        cursor.execute(query, (username,))
+        result = cursor.fetchone()
+        return result
+
+    def getRewardsPointsByUsername(self, username):
+        cursor = self.conn.cursor()
+        query = "select reward_points from users where username = %s;"
+        cursor.execute(query, (username,))
+        result = cursor.fetchone()
+        return result
+
+    def getTargetGoalAmountByUsername(self, username):
+        cursor = self.conn.cursor()
+        query = "select target_goal_amount from users where username = %s;"
+        cursor.execute(query, (username,))
+        result = cursor.fetchone()
+        return result
+
+    def getTargetGoalHoursByUsername(self, username):
+        cursor = self.conn.cursor()
+        query = "select target_goal_hours from users where username = %s;"
+        cursor.execute(query, (username,))
+        result = cursor.fetchone()
+        return result
+
+    def getIsAdminByUsername(self, username):
+        cursor = self.conn.cursor()
+        query = "select is_admin from users where username = %s;"
+        cursor.execute(query, (username,))
+        result = cursor.fetchone()
+        return result
+
     def insertUser(self, username, password, reward_points, target_goal_amount, target_goal_hours, is_admin):
         cursor = self.conn.cursor()
         query = "insert into users(username, password, reward_points, target_goal_amount, target_goal_hours, is_admin) values (%s, %s, %s, %s, %s, %s) returning user_id;"
@@ -42,6 +84,48 @@ class UserDAO:
         cursor = self.conn.cursor()
         query = "update users set password = %s, reward_points = %s, target_goal_amount = %s, target_goal_hours = %s, is_admin = %s where user_id = %s;"
         cursor.execute(query, (password, reward_points, target_goal_amount, target_goal_hours, is_admin, user_id))
+        self.conn.commit()
+        return user_id
+
+    def updateUserUsername(self, user_id, username):
+        cursor = self.conn.cursor()
+        query = "update users set username = %s where user_id = %s;"
+        cursor.execute(query, (username, user_id))
+        self.conn.commit()
+        return user_id
+
+    def updateUserPassword(self, user_id, password):
+        cursor = self.conn.cursor()
+        query = "update users set password = %s where user_id = %s;"
+        cursor.execute(query, (password, user_id))
+        self.conn.commit()
+        return user_id
+
+    def updateUserRewardPoints(self, user_id, reward_points):
+        cursor = self.conn.cursor()
+        query = "update users set reward_points = %s where user_id = %s;"
+        cursor.execute(query, (reward_points, user_id))
+        self.conn.commit()
+        return user_id
+
+    def updateUserTargetGoalAmount(self, user_id, target_goal_amount):
+        cursor = self.conn.cursor()
+        query = "update users set target_goal_amount = %s where user_id = %s;"
+        cursor.execute(query, (target_goal_amount, user_id))
+        self.conn.commit()
+        return user_id
+
+    def updateUserTargetGoalHours(self, user_id, target_goal_hours):
+        cursor = self.conn.cursor()
+        query = "update users set target_goal_hours = %s where user_id = %s;"
+        cursor.execute(query, (target_goal_hours, user_id))
+        self.conn.commit()
+        return user_id
+
+    def updateUserIsAdmin(self, user_id, is_admin):
+        cursor = self.conn.cursor()
+        query = "update users set is_admin = %s where user_id = %s;"
+        cursor.execute(query, (is_admin, user_id))
         self.conn.commit()
         return user_id
 
