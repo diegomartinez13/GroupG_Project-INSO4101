abcc6df814a4679c66fc5fa746a702feec54e1db
Diego M.G <diego.martinez13@upr.edu>
2023-04-01 19:46:01 -0400

    Authentification base code

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import os
 
 from dotenv import load_dotenv
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, redirect
 from flask_cors import CORS
+# from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from backend.controller.events import BaseEvents
 from backend.controller.users import BaseUsers
@@ -14,9 +16,10 @@ load_dotenv(dotenv_path=".env")
 
 CORS(app)
 
-@app.route("/")
-def index():
-    return app.send_static_file('index.html')
+# NO NEED FOR A ROUTE TO HOME
+# @app.route("/")
+# def index():
+#     return app.send_static_file('index.html')
 
 @app.route('/<path:path>')
 def static_file(path):
@@ -26,27 +29,45 @@ def static_file(path):
 def getEvents():
     return BaseEvents().getAllEvents()
 
-@app.route("/login", methods=['POST'])
+@app.route("/profile/login", methods=['POST', 'GET'])
 def login():
-    username = request.json['username']
-    password = request.json['password']
+    # if request.method == 'POST':
+    username = request.form.get('username')
+    password = request.form.get('password')
+    
     user = BaseUsers().getUserByUsername(username)
+    
     if user:
-        if user.json['password'] == password:
+        if check_password_hash(user.json['password'], password):
+            # login_user(user, remember=True)
             return user
         else:
             return jsonify(Error="Invalid Password"), 401
     else:
         return jsonify(Error="User Not Found"), 404
 
-@app.route("/register", methods=['POST'])
+@app.route("/profile/register", methods=['POST', 'GET'])
 def register():
-    username = request.json['username']
-    password = request.json['password']
+    # if request.method == 'POST':
+    username = request.form.get('username')
+    password = request.form.get('password')
+    password_confirmation = request.form.get('password_confirmation')
+    
     if BaseUsers().checkUserExists(username):
         return jsonify(Error="User Already Exists"), 409
+    elif password != password_confirmation:
+        return jsonify(Error="Passwords do not match"), 401
     else:
-        return BaseUsers().insertUser(username, password, 0, 0, 0, False)
+        new_user = BaseUsers().insertUser(username, generate_password_hash(password, method='sha256'), 0, 0, 0, False)
+        # TODO: database connection implementation
+        # login_user(new_user, remember=True)
+        return jsonify(new_user), 200
+
+# @app.route('/profile/logout')
+# @login_required
+# def logout():
+#     logout_user()
+#     return redirect('/profile')
 
 if __name__ == "__main__":
     app.run()
