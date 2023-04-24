7260e096342aede687144cf3a14d055f40605645
Daniel7-28 <juan34daniel@gmail.com>
2023-04-24 19:35:15 -0400

    ControllerPostsFixed

--- backend/controller/posts.py
+++ backend/controller/posts.py
@@ -6,7 +6,7 @@ from backend.model.posts import PostsDAO
 class BasePosts:
     def build_map_dict(self, row):
         result = {'post_id': row[0], 'title': row[1], 'content': row[2], 'created_at': row[3],
-                  'parent_post_id': row[4], 'user_id': row[5], 'is_admin': row[6]}
+                  'parent_post_id': row[4], 'user_id': row[5]}
         return result
 
     def build_map_dict_tittle(self, row):
